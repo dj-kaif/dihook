236ccd8f77e88c9a3c4f5a4331c570d999f3bab0
dj-kaif <djkaifdev@gmail.com>
2026-06-12 15:50:04 +0600

    Add modify method to change webhook name and avatad

--- dihook/dihook.py
+++ dihook/dihook.py
@@ -1,4 +1,6 @@
 import requests
+import base64
+import mimetypes
 
 class Dihook:
   def __init__(self, url: str, username: str = None, avatar: str = None):
@@ -10,8 +12,7 @@ class Dihook:
     self.text = text
     payload = {
       "content": text
-  }
-
+    }
     active_username = username or self.username
     active_avatar = avatar or self.avatar
 
@@ -23,7 +24,52 @@ class Dihook:
     try:
       response = requests.post(self.url, json=payload)
       response.raise_for_status()
-      return "succes!"
+      return "Webhook sent!"
     
     except Exception as e:
       return f"An error occurred: {e}"
+
+  def modify(self, name: str = None, avatar: str = None):
+    payload = {}
+
+    if name:
+      payload["name"] = name
+
+    if avatar:
+      if avatar.startswith(("http://", "https://")):
+        try:
+          img_response = requests.get(avatar)
+          img_response.raise_for_status()
+          image_bytes = img_response.content
+          mime_type = img_response.headers.get("Content-Type", "image/png")
+
+        except Exception as e:
+          return f"Failed to download image from URL: {e}"
+
+      else:
+        mime_type, _ = mimetypes.guess_type(avatar)
+        if not mime_type:
+          mime_type = "image/png"
+
+        try:
+          with open(avatar, "rb") as image_file:
+            image_bytes = image_file.read()
+
+        except Exception as e:
+          return f"Failed to read local file: {e}"
+
+      encoded_string = base64.b64encode(image_bytes).decode('utf-8')
+      payload["avatar"] = f"data:{mime_type};base64,{encoded_string}"
+
+    try:
+      response = requests.patch(self.url, json=payload)
+      response.raise_for_status()
+
+      if name:
+        self.username = name
+      if avatar:
+        self.avatar = avatar
+      return "Webhook settings updated permanently on Discord!"
+
+    except Exception as e:
+      return f"An error occurred while modifying: {e}"
--- main.py
+++
@@ -1,6 +0,0 @@
-from dihook import Dihook
-
-WEBHOOK = Dihook("https://discord.com/api/webhooks/1510197351690797147/n8kgLDQ8LMyirUT4rp7pknfBIny4fDR5d2h0yoOJYgD5gtcbIkPb2G_DbYNeUvQAAwqV")
-
-
-print(WEBHOOK.send("Hello!", "Dihook"))
